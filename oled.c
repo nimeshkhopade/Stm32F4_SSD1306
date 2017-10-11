0183d46e6fb469d38a39fef83e2bec45dde925ba
nimeshkhopade <nimeshkhopade01@gmail.com>
2017-10-12 01:34:20 +0530

    First Commit

--- oled.c
+++ oled.c
@@ -1,8 +1,10 @@
 #include <stm32f4xx.h>
-long PD12, PD13, PD14, PD15;
+int PD12, PD13, PD14, PD15, PA8, PA0, PA1, PA2;
+int i = 0, count = 0,x=0, arr[1000], data=0;
+uint32_t* p;
 void delay(){
 	int j = 0;
-	for(int i = 0;i < 50000; i++)
+	for(i = 0;i < 50000; i++)
 	{
 		for(j = 0; j < 500; j++){}
 	}
@@ -18,23 +20,13 @@ int main(void)
           | FLASH_ACR_ICEN                      // instruction cache enable *
           | FLASH_ACR_DCEN;                     // data cache enable 
 	
-	/* Configure clocks
-	 * Max SYSCLK: 168MHz
-	 * Max AHB:  SYSCLK
-	 * Max APB1: SYSCLK/4 = 48MHz
-	 * Max APB2: SYSCLK/2 = 86MHz
-	 * + enable sys clock output 2 with clock divider = 4 
-	*/
-	/*
-	RCC->CFGR =
-          0x0                           // Clock output 2 is SYSCLK (RCC_CFGR_MCO2) 
-        | ( 0x6 << 27)                  // Clock output divider 
-        | RCC_CFGR_PPRE2_DIV2           // APB2 prescaler 
-        | RCC_CFGR_PPRE1_DIV4;          // APB2 prescaler 
-				*/
 	
+	/********* CLOCK GENERATION **********/
 	
-	/* Clock control register */
+	RCC->CFGR = RCC_CFGR_PPRE2_DIV2           // APB2 prescaler 
+								| RCC_CFGR_PPRE1_DIV4;          // APB1 prescaler 
+				
+	
 	RCC->CR = RCC_CR_HSEON;         /* Enable external oscillator */
 
 	/* Wait for locked external oscillator */
@@ -47,13 +39,6 @@ int main(void)
         | (168 << 6)                            /* PLL multiplication */
         | (0 << 16)                             /* PLL sys clock division */
         | (7 << 24);                            /* PLL usb clock division =48MHz */
-
-	/* crystal:  8MHz
-	 * PLL in:   2MHz (div 4)
-	 * PLL loop: 336MHz (mul 168)
-	 * PLL out:  168MHz (div 2)
-	 * PLL usb:  48MHz (div 7)
-	*/
 	
 	RCC->CR |= RCC_CR_PLLON;		/* Enable PLL */
 
@@ -64,40 +49,38 @@ int main(void)
 	RCC->CFGR |= RCC_CFGR_SW_PLL | RCC_CFGR_PPRE1_DIV4 | RCC_CFGR_PPRE2_DIV2;   /* SYSCLK is PLL */
 	
 	while((RCC->CFGR & RCC_CFGR_SW_PLL) != RCC_CFGR_SW_PLL);		/* Wait for SYSCLK to be PLL */
+	/************ /CLOCK GENERATION ************/
 
-	RCC->AHB1ENR |= 1 << 3;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
 	
-	GPIOD->MODER   = GPIO_MODER_MODE15_0|GPIO_MODER_MODE14_0|GPIO_MODER_MODE13_0|GPIO_MODER_MODE12_0;		//0x55 << 16; /* output */
+	RCC->APB1ENR = 1 << 21; //I2C1 ON
 	
-	int x =12;
+	GPIOB->AFR[0] |= 4 << 24;
 	
+	GPIOB->AFR[1] |= 4 << 4;
+	
+	GPIOB->MODER |= 2 << 12 | 2 << 18;
+	
+	/*
+	ADC->CCR = 1 << 16; //ADCPRE is set to 4 i.e APB2 / 4 = 21MHz for ADCCLK
+	
+	ADC1->SMPR2 = 6 << 0 | 6 << 3 | 6 << 6; //Sampling time set to 144 cycles for channel 0
+	
+	ADC1->CR1 |= 0 << 24 | 1 << 8; //12bit resolution|Scan Mode
+	
+	ADC1->CR2 = 1 << 0 | 1 << 1 | 1 << 10; // ADON, CONT, EOCS
+	
+	ADC1->SQR3 = 0 << 0; //| 1 << 5 | 2 << 10 ; // put channel number(CH0,CH1,CH2)
+	
+	ADC1->SQR1 = 1 << 20; //L = 1 conversions (single adc conversion)
+	
+	ADC1->CR2 |= ADC_CR2_SWSTART; //start conversion
+	*/
+
 	while(1){
-	delay();
-	GPIOD->ODR ^= 0xf000;
-	delay();
-	GPIOD->ODR ^= GPIOD->ODR;
-	delay();
-	for(int k = 0; k < 4; k++){
-		delay();
-		GPIOD->ODR ^= 1 << x;
-		PD12 = GPIOD->ODR & 0x00001000;
-		PD13 = GPIOD->ODR & 0x00002000;
-		PD14 = GPIOD->ODR & 0x00004000;
-		PD15 = GPIOD->ODR & 0x00008000;
-		delay();
-		x++;
-	}
-	GPIOD->ODR ^= GPIOD->ODR;
-	for(int k = 0; k < 4; k++){
-		x--;
-		delay();
-		GPIOD->ODR ^= 1 << x;
-		PD12 = GPIOD->ODR & 0x00001000;
-		PD13 = GPIOD->ODR & 0x00002000;
-		PD14 = GPIOD->ODR & 0x00004000;
-		PD15 = GPIOD->ODR & 0x00008000;
-		delay();
-	}
-	GPIOD->ODR ^= GPIOD->ODR;
-} 
+	/*
+		while(ADC_SR_EOC == 0); //wait for conversion to over.
+	PA0 = ADC1->DR; */
+		
+	} 
 }
